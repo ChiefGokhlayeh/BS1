3f6f1e2b0af76acb425883d67e162bae280474b5
Andreas Baulig <andreas.baulig@gmail.com>
2019-05-08 15:48:46 +0200

    Add stub for lab2 appstart program

---
+++ lab2/appstart/src/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <sys/signal.h>
+#include <sys/wait.h>
+#include <sys/types.h>
+
+#include <unistd.h>
+
+int main(void)
+{
+    return EXIT_SUCCESS;
+}
