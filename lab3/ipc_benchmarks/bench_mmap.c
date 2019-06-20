d0604d77ca0352de26311e608e8ed729b7c91e83
Andreas Baulig <andreas.baulig@gmail.com>
2019-06-20 16:38:28 +0200

    Increase sizes array to 64MiB

--- lab3/ipc_benchmarks/bench_mmap.c
+++ lab3/ipc_benchmarks/bench_mmap.c
@@ -24,7 +24,10 @@
 
 int main(int argc, char *argv[])
 {
-    const int sizes[] = {128, 256, 512, 1024, 2048, 4096, 8192, 16384, 32768, 65536, 131072, 262144, 524288};
+    const int sizes[] = {
+        128, 256, 512, 1024, 2048, 4096, 8192, 16384, 32768,
+        65536, 131072, 262144, 524288, 1048576, 2097152,
+        4194304, 8388608, 16777216, 33554432, 67108864};
     const int sizes_num = sizeof(sizes) / sizeof(sizes[0]);
 #define MAX_SIZE sizes[sizes_num - 1]
     pid_t pid = getpid();
