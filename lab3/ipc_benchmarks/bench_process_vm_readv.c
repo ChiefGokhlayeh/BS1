c775d6b4d1b8dd9d22448360a84807133a53b166
Andreas Baulig <andreas.baulig@gmail.com>
2019-06-20 16:19:42 +0200

    Apply code formatter

--- lab3/ipc_benchmarks/bench_process_vm_readv.c
+++ lab3/ipc_benchmarks/bench_process_vm_readv.c
@@ -2,7 +2,7 @@
  * Small benchmark of Linux' process_vm_readv / process_vm_writev calls,
  * see man 2 process_vm_readv
  * These two function calls were added in Linux v3.2
- * 
+ *
  * Author: Rainer Keller, HS Esslingen
  */
 #define _GNU_SOURCE
@@ -22,13 +22,14 @@
 
 #include "bench_utils.h"
 
-int main(int argc, char * argv[]) {
+int main(int argc, char *argv[])
+{
     const int sizes[] = {128, 256, 512, 1024, 2048, 4096, 8192, 16384, 32768, 65536, 131072, 262144, 524288};
-    const int sizes_num = sizeof(sizes)/sizeof(sizes[0]);
-#define MAX_SIZE sizes[sizes_num-1]
+    const int sizes_num = sizeof(sizes) / sizeof(sizes[0]);
+#define MAX_SIZE sizes[sizes_num - 1]
     int pipe_child_to_parent[2];
     int pipe_parent_to_child[2];
-    char * buffer;
+    char *buffer;
     pid_t pid;
     pid_t pid_child;
     int ret;
@@ -46,76 +47,78 @@ int main(int argc, char * argv[]) {
     if (-1 == ret)
         ERROR("fork", errno);
 
-    if (0 == ret) {
+    if (0 == ret)
+    {
         /* CHILD Process */
         int num_written;
         int to_write;
-        char ** ptr;
+        char **ptr;
 
         pid = getpid();
         DEBUG(printf("PID:%d (CHILD) starts\n",
-                     (int) pid));
+                     (int)pid));
 
         // close the reading end in child_to_parent and writing end in parent_to_child
-        ret = close (pipe_child_to_parent[0]);
-        ret = close (pipe_parent_to_child[1]);
+        ret = close(pipe_child_to_parent[0]);
+        ret = close(pipe_parent_to_child[1]);
 
         buffer = malloc(MAX_SIZE);
         if (NULL == buffer)
             ERROR("malloc", ENOMEM);
-        memset (buffer, 0, MAX_SIZE);
+        memset(buffer, 0, MAX_SIZE);
 
         // Write the child's buffer address to the parent
         ptr = &buffer;
-        for (num_written = 0, to_write=sizeof(char*); num_written < sizeof(char*); num_written += ret, to_write -= ret)
-            ret = write (pipe_child_to_parent[1], ptr+num_written, to_write);
+        for (num_written = 0, to_write = sizeof(char *); num_written < sizeof(char *); num_written += ret, to_write -= ret)
+            ret = write(pipe_child_to_parent[1], ptr + num_written, to_write);
 
         DEBUG(printf("PID:%d (CHILD) waits\n",
-                     (int) pid));
+                     (int)pid));
         ret = read(pipe_parent_to_child[0], &num_written, 1);
         DEBUG(printf("PID:%d (CHILD) exits\n",
-                     (int) pid));
+                     (int)pid));
 
-        exit (EXIT_SUCCESS);
+        exit(EXIT_SUCCESS);
     }
     int num_read;
     int to_read;
-    char * remote_buffer;
-    char ** ptr;
-    int * ticks;
+    char *remote_buffer;
+    char **ptr;
+    int *ticks;
     struct iovec local[1];
     struct iovec remote[1];
     int i;
     DEBUG(printf("PID:%d (PARENT) starts child_pid:%d\n",
-                 (int) pid, (int)pid_child));
+                 (int)pid, (int)pid_child));
     // close the reading end in parent_to_child and writing end in child_to_parent
-    ret = close (pipe_parent_to_child[0]);
-    ret = close (pipe_child_to_parent[1]);
+    ret = close(pipe_parent_to_child[0]);
+    ret = close(pipe_child_to_parent[1]);
 
     // Read the child's buffer_address
     ptr = &remote_buffer;
-    for (num_read = 0, to_read = sizeof(char*); num_read < sizeof(char*); num_read += ret, to_read -= ret)
-        ret = read(pipe_child_to_parent[0], ptr+num_read, to_read);
+    for (num_read = 0, to_read = sizeof(char *); num_read < sizeof(char *); num_read += ret, to_read -= ret)
+        ret = read(pipe_child_to_parent[0], ptr + num_read, to_read);
 
     DEBUG(printf("PID:%d (PARENT) remote_buffer:%p\n",
-                 (int) pid, remote_buffer));
+                 (int)pid, remote_buffer));
 
     buffer = malloc(MAX_SIZE);
     if (NULL == buffer)
         ERROR("malloc", ENOMEM);
-    memset (buffer, 0, MAX_SIZE);
-        
-    ticks = malloc (MEASUREMENTS * sizeof (int));
+    memset(buffer, 0, MAX_SIZE);
+
+    ticks = malloc(MEASUREMENTS * sizeof(int));
     if (NULL == ticks)
         ERROR("malloc", ENOMEM);
-    memset (ticks, 0, MEASUREMENTS * sizeof(int));
+    memset(ticks, 0, MEASUREMENTS * sizeof(int));
 
     /* PARENT Process: WRITES into other process' memory! */
     memset(buffer, 'a', MAX_SIZE);
-    local[0].iov_base = (void *) buffer;
-    remote[0].iov_base = (void *) remote_buffer;
+    local[0].iov_base = (void *)buffer;
+    remote[0].iov_base = (void *)remote_buffer;
 
-    for (i = 0; i < sizes_num; i++) {
+    for (i = 0; i < sizes_num; i++)
+    {
         int current_size = sizes[i];
         int nwrite;
         int j;
@@ -132,13 +135,14 @@ int main(int argc, char * argv[]) {
         remote[0].iov_len = current_size;
 
         gettimeofday(&tv_start, NULL);
-        for (j = 0; j < MEASUREMENTS; j++) {
+        for (j = 0; j < MEASUREMENTS; j++)
+        {
             unsigned long long start;
             unsigned long long stop;
             start = getrdtsc();
             nwrite = process_vm_writev(pid_child, local, 1, remote, 1, 0);
             stop = getrdtsc();
-            assert (nwrite == current_size);
+            assert(nwrite == current_size);
             ticks[j] = stop - start;
         }
         gettimeofday(&tv_stop, NULL);
@@ -146,9 +150,12 @@ int main(int argc, char * argv[]) {
         min_ticks = INT_MAX;
         max_ticks = INT_MIN;
         ticks_all = 0;
-        for (j = 0; j < MEASUREMENTS; j++) {
-            if (min_ticks > ticks[j]) min_ticks = ticks[j];
-            if (max_ticks < ticks[j]) max_ticks = ticks[j];
+        for (j = 0; j < MEASUREMENTS; j++)
+        {
+            if (min_ticks > ticks[j])
+                min_ticks = ticks[j];
+            if (max_ticks < ticks[j])
+                max_ticks = ticks[j];
             ticks_all += ticks[j];
         }
         ticks_all -= min_ticks;
@@ -158,8 +165,8 @@ int main(int argc, char * argv[]) {
 
         printf("PID:%d time: min:%d max:%d Ticks Avg without min/max:%f Ticks (for %d measurements) for %d Bytes (%.2f MB/s)\n",
                pid, min_ticks, max_ticks,
-               (double) ticks_all / (MEASUREMENTS - 2.0), MEASUREMENTS, nwrite,
-               ((double) current_size*MEASUREMENTS)/(1024.0*1024.0*time_delta_sec));
+               (double)ticks_all / (MEASUREMENTS - 2.0), MEASUREMENTS, nwrite,
+               ((double)current_size * MEASUREMENTS) / (1024.0 * 1024.0 * time_delta_sec));
     }
 
     // Tell Child to exit, too:
